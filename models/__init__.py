19324532ffb82f92228fa2faeb10d24c99fa8745
GigiBomba <BonjourLOL444@Gmail.com>
2026-06-04 20:20:51 +0300

    Remove unused models package (dead code: zero imports across codebase)

--- models/__init__.py
+++
@@ -1,6 +0,0 @@
-from models.trip import Trip
-from models.route import Route, RouteStop
-from models.truck import Truck
-from models.driver import Driver
-
-__all__ = ["Trip", "Route", "RouteStop", "Truck", "Driver"]
